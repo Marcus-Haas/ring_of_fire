b8e3a654b0eebb4bc2b5fcb38b3198456f24a2cf
Marcus-Haas <marcus-haas@hotmail.de>
2024-04-20 15:08:15 +0200

    add firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), importProvidersFrom(provideFirebaseApp(() => initializeApp({ "projectId": "ring-of-fire-60fd9", "appId": "1:56845266036:web:b9744e0631d1ebff1af521", "storageBucket": "ring-of-fire-60fd9.appspot.com", "apiKey": "AIzaSyDKN6nLg_YyR9i1jLHCEs7xTSho6ODW6sI", "authDomain": "ring-of-fire-60fd9.firebaseapp.com", "messagingSenderId": "56845266036" }))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
